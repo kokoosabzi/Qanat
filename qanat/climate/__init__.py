8b0b4fc92f621e5a37a97ab263ee7a7345cabf23
kokoosabzi <kokoosabzi@gmail.com>
2026-09-15 23:27:31 +0330

    feat(climate): export watershed integration API

--- qanat/climate/__init__.py
+++ qanat/climate/__init__.py
@@ -1,5 +1,6 @@
 from .engine import ClimateEngine, ClimateResult
 from .providers import ClimateProvider, DailyWeather, OpenMeteoProvider
+from .watershed import WatershedIndicators, watershed_indicators
 
 __all__ = [
     "ClimateEngine",
@@ -7,4 +8,6 @@ __all__ = [
     "ClimateProvider",
     "DailyWeather",
     "OpenMeteoProvider",
+    "WatershedIndicators",
+    "watershed_indicators",
 ]
